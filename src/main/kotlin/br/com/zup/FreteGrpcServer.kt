736afaba2d12851b7df3aafa36aa04dd59a085d7
caroldf07 <carolina.df@outlook.com>
2021-04-09 15:26:27 -0300

    Finalizada teoria e demonstrações sobre gRPC

--- src/main/kotlin/br/com/zup/FreteGrpcServer.kt
+++ src/main/kotlin/br/com/zup/FreteGrpcServer.kt
@@ -1,5 +1,6 @@
 package br.com.zup
 
+import io.grpc.Status
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
 import javax.inject.Singleton
@@ -12,6 +13,14 @@ class FreteGrpcServer : FreteServiceGrpc.FreteServiceImplBase() {
     private val logger = LoggerFactory.getLogger(FreteGrpcServer::class.java)
     override fun calculaFrete(request: CalculaFreteRequest?, responseObserver: StreamObserver<CalculaFreteResponse>?) {
         logger.info("Calculando frete para request: $request")
+
+        val cep = request!!.cep
+
+        if (cep == null || cep.isBlank()) {
+            val erro = Status.INVALID_ARGUMENT.withDescription("Cep deve ser informado").asRuntimeException()
+
+            responseObserver?.onError(erro)
+        }
 
         val response = CalculaFreteResponse.newBuilder()
             .setCep(request!!.cep)
